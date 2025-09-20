ba9f13f40ddb9b0c5e8ca7e20faa62b6fda8b96f
willblood <ledmw19@gmail.com>
2025-09-20 01:24:29 +0000

    Done Chat page layout

---
+++ app/components/ChatPreview.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  li(class="px-4 py-3 flex items-center bg-barca-blue text-white hover:bg-yellow-500 cursor-pointer transition")
+    img.w-12.h-12.rounded-full.border.border-gray-300(
+      src="https://ui-avatars.com/api/?name=Messi&background=random&size=64"
+    )
+    .ml-3
+      p.font-semibold Lionel Messi
+      p.text-sm Last message preview...
+</template>
